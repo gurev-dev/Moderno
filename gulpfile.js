eb04b4c11577e50bda3dcb05742811b607bc77b6
stanislav-VG <stant3ch@yandex.ru>
2020-09-26 14:05:35 +0700

    added popup-login

--- gulpfile.js
+++ gulpfile.js
@@ -48,10 +48,10 @@ gulp.task('browser-sync', function () {
 gulp.task('script', function () {
 	return gulp.src([
 		'node_modules/slick-carousel/slick/slick.js',
-		'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
 		'node_modules/mixitup/dist/mixitup.js',
 		'node_modules/rateyo/src/jquery.rateyo.js',
 		'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
+		'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
 	])
 		.pipe(concat('libs.min.js'))
 		.pipe(uglify())
@@ -72,9 +72,9 @@ gulp.task('style', function () {
 	return gulp.src([
 		'node_modules/normalize.css/normalize.css',
 		'node_modules/slick-carousel/slick/slick.css',
-		'node_modules/magnific-popup/dist/magnific-popup.css',
 		'node_modules/rateyo/src/jquery.rateyo.css',
 		'node_modules/ion-rangeslider/css/ion.rangeSlider.css',
+		'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.css',
 	])
 		.pipe(concat('libs.min.css'))
 		.pipe(cssmin())
@@ -90,7 +90,6 @@ gulp.task('imagemin', function () {
 			svgoPlugins: [{ removeViewBox: false }]
 		}))
 		.pipe(gulp.dest('app/images/'))
-	// .pipe(browserSync.reload({ stream: true }))
 });
 gulp.task('webp', function () {
 	return gulp.src('#src/**/*.{jpg,png,gif,svg,ico}')
